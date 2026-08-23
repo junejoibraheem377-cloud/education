6f1687acbb320d5393fd134f78b091b563505a99
Ibrahim Junejo <junejoibraheem377@gmail.com>
2026-08-23 22:39:07 +0500

    Add toggleDetails function to script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
- document.querySelector(".hero button").addEventListener("click", () => {
+ 
+document.querySelector(".hero button").addEventListener("click", () => {
   alert("Welcome! Courses coming soon.");
 });
+function toggleDetails(id) {
+  const desc = document.getElementById(id);
+  desc.classList.toggle("show");
+}
